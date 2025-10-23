539e444f14db5f3468d167bdf7187fd4b6d6696a
Rama Wandya <ramawandia153@gmail.com>
2025-10-23 20:18:36 +0700

    fix(test_numcount): fix test number 4

--- logicaltest/task5_numCount_test.go
+++ logicaltest/task5_numCount_test.go
@@ -13,7 +13,7 @@ func TestNumCount(t *testing.T) {
 		{"this should be 4", []string{"2", "h", "6", "u", "y", "t", "7", "j", "y", "h", "8"}, 4},
 		{"this should be 5", []string{"b", "7", "h", "6", "h", "k", "i", "5", "g", "7", "8"}, 5},
 		{"this should be 7", []string{"7", "b", "8", "5", "6", "9", "n", "f", "y", "6", "9"}, 7},
-		{"this should be 7", []string{"u", "h", "b", "n", "7", "6", "5", "1", "g", "7", "9"}, 6},
+		{"this should be 6", []string{"u", "h", "b", "n", "7", "6", "5", "1", "g", "7", "9"}, 6},
 	}
 
 	for _, tt := range testTable {
